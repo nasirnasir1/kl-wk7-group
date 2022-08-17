748a61d65c6361576562577880948c081165118e
cceliuss187 <cceliuss187@gmail.com>
2022-08-16 20:58:11 -0400

    this is my second commit

--- main.sh
+++ main.sh
@@ -15,7 +15,25 @@ echo "(B) Itsy Bitsy Spider"
 echo "(C) Mary Had a Little Lamb"
 
 
-read 
+read -r a
+if [[ $a=A ]]
+then
+	echo "$NR1"
+	echo "Jack and Jill went up the hill"
+        echo"To fetch a pail of water."
+	echo "Jack fell down and broke his crown,"
+	echo "And Jill came tumbling after."
+	echo "Then up got Jack and said to Jill,"
+	echo "As in his arms he took her,"
+	echo "“Brush off that dirt for you’re not hurt,"
+	echo "Let’s fetch that pail of water.”"
+	echo "So Jack and Jill went up the hill"
+	echo "To fetch the pail of water,"
+	echo "And took it home to Mother dear,"
+	echo "Who thanked her son and daughter."
+
+fi
+
 
 
 
