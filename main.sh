13a93ea3e41bd7de2507a1f46a198ae2641d4f21
cceliuss187 <cceliuss187@gmail.com>
2022-08-16 20:22:16 -0400

    this is my first commit

--- main.sh
+++ main.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+# Creator:           Caleb Celius
+# Game_Title:        Nursery Rhymes Guess
+# Game_Description:  This game provides you with only piece of a nursery rhyme and you are to guess the other pieces by selecting what words you thing should come next. 
+# Game_Version:      1.0
+
+NR1="Jack and Jill"
+NR2="Itsy Bitsy Spider"
+NR3="Mary Had a Little Lamb"
+
+echo "-----------Select a nursery rhymes-------------"
+echo "(A) Jack and Jill"
+echo "(B) Itsy Bitsy Spider"
+echo "(C) Mary Had a Little Lamb"
+
+
+read 
+
+
+
+
+
+
+
+
+
+
+
+
